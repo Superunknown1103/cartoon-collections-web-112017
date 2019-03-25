ad8c2f60f39bcfeff46da6387a8aaf77e89b83bd
Bruce Tsvilikhovsky <bruce.tsvilikhovsky@flatironschool.com>
2019-03-25 15:11:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,9 +9,7 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.map{|c|
-    if (c.length < 4)
-      return false }
+
 end
 
 def find_the_cheese# code an argument here
